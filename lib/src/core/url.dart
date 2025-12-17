599c387b26cfa82678dcecf8157a29a0b8af3039
Albin Antony <albinantony2420@gmail.com>
2025-12-17 18:40:52 +0530

    Revert "test addedd in urls.dart"

--- lib/src/core/url.dart
+++ lib/src/core/url.dart
@@ -2,6 +2,4 @@ class Url {
   static String baseUrl = 'http://198.169.1.56:8100';
   static String auth = 'user/auth';
   static String refresh = "user/refresh";
-
-  static String test = "/test";
 }
